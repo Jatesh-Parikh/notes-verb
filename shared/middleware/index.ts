e3c3b8418cc83cede1289257f5693d8f80804640
Jatesh Parikh <jateshparikh97@gmail.com>
2026-01-19 15:51:59 +0530

    add shared/middleware/asyncHandler

--- shared/middleware/index.ts
+++ shared/middleware/index.ts
@@ -14,7 +14,13 @@ declare global {
 
 export function authenticateToken() {};
 
-export function asyncHandler() {};
+export function asyncHandler(
+    fn: (req: Request, res: Response, next: NextFunction) => Promise<any>
+) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(fn(req, res, next)).catch(next);
+    };
+};
 
 export function validateRequest(schema: any) {
     return (req: Request, res: Response, next: NextFunction) => {
